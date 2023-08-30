b292ac29ec1fe904cedfdeeba7ec04a176295c88
Erick Menezes <erick.henrique@me.com>
2023-08-30 10:54:14 -0300

    Implementado script multlinguas

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print ("Hello World!"))
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+
+# Aqui aplicamos a condicional
+
+if current_language == "pt_BR":
+	msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+	msg = "Ciao, Mondo!"
+elif current_language == "fr_FR":
+	msg = "Bonjour, Monde!"
+
+print(msg)
